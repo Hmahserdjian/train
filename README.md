bb54eceb148cf10476f9144ab5e178cec4a7df1e
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-04-10 20:52:58 -0400

    created html

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # train
+
+built my html made it with bootstrap. ive added all my jquery, bootstrap, javascript and, firebase script and link tags.
+
+I used jumbotron, made an form tag and an table tag using bootstrap. 
---
+++ assets/javascript.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCnyADpZ-3ehEcqhR1ubiM3B82dq-k5wd0",
+    authDomain: "train-9d5ac.firebaseapp.com",
+    databaseURL: "https://train-9d5ac.firebaseio.com",
+    projectId: "train-9d5ac",
+    storageBucket: "",
+    messagingSenderId: "60042903659"
+};
+firebase.initializeApp(config);
