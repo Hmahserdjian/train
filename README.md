cc395e29573222e163f3c0678ce2d808fd4b3ed1
Hrag Mahserdjian <hmahserdjian.hm@gmail.com>
2018-04-12 21:54:05 -0400

    final

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 built my html made it with bootstrap. ive added all my jquery, bootstrap, javascript and, firebase script and link tags.
 
 I used jumbotron, made an form tag and an table tag using bootstrap. 
+I coded everything out my tutor help me alot. now im having trouble getting my real time to work correctly in my console.log everything looks fine.  
+
+
+Got my real time to work.
+
--- assets/javascript.js
+++ assets/javascript.js
@@ -7,3 +7,112 @@ var config = {
     messagingSenderId: "60042903659"
 };
 firebase.initializeApp(config);
+
+var database = firebase.database();
+$("#train-time").on("click", function (event) {
+    event.preventDefault();
+
+    var trainName = $("#trainName").val().trim();
+    var dEstination = $("#dEstination").val().trim();
+    var firstTraintime = moment($("#firstTraintime").val().trim(), "HH.mm.ss").format("HH.mm.ss");
+    var arrive1 = moment($("#arrive1").val().trim(), "HH.MM".format("HH.MM") )
+    var fRequency = $("#fRequency").val().trim();
+    var minutesAway = $("#minutesAway").val().trim();
+
+    var newTrain = {
+        name: trainName,
+        destination: dEstination,
+        arrival: firstTraintime,
+        rate: fRequency,
+        away: minutesAway,
+    };
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.arrival);
+    console.log(newTrain.rate);
+    console.log(newTrain.away);
+
+    database.ref().push(newTrain);
+
+
+
+});
+
+database.ref().on("child_added", function (childSnapshot) {
+   var name = childSnapshot.val().name;
+    console.log(name);
+    var destination =childSnapshot.val().destination;
+    console.log(destination);
+    var arrival = childSnapshot.val().arrival;
+    console.log(arrival);
+    var rate = childSnapshot.val().rate;
+    console.log(rate);
+    var away = childSnapshot.val().away;
+    console.log(away);
+    var arrive1 = childSnapshot.val().arrive1;
+    console.log(arrive1);
+
+// to display times correctly in the table use logic below.
+    var firstTraintimeConverted = moment(firstTraintime, "HH:mm").subtract(1, "years");
+    console.log(firstTraintime);
+
+    var realTime = moment();
+    console.log("CURRENT TIME: " + moment(realTime).format("HH:mm"));
+
+    var timeDifference = moment().diff(moment(realTime), "minutes");
+    console.log("Difference in time: " + timeDifference);
+
+    var minutesAway = fRequency - minutesAway;
+    console.log("minutes till Train: " + minutesAway);
+
+    
+    var arrive1 = moment().add(minutesAway, 'minutes').format('hh:mm:ss');;
+    console.log("arrive1: " + arrive1);
+
+
+
+    var tillNext = timeDifference % fRequency;
+    console.log(tillNext);
+ 
+    var tillNext = fRequency - tillNext;
+    console.log("MINUTES TILL TRAIN: " + tillNext);
+
+    var nextTrain = moment().add(tillNext, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm")); 
+
+    $("#tBody").append(
+        "<tr>" +
+            "<td>" + name + "</td>" +
+            "<td>" + destination + "</td>" +
+            "<td>" + arrival + "</td>" +
+            "<td>" + arrive1 + "</td>" +
+            "<td>" + rate + "</td>" +
+            "<td>" + away + "</td>" +
+        "</tr>"
+    )
+});
+
+
+// var firstTraintimeConverted = moment(firstTraintime, "HH:mm").subtract(1, "years");
+// console.log(firstTraintime);
+
+// var realTime = moment();
+// console.log("CURRENT TIME: " + moment(realTime).format("HH:mm"));
+
+// var timeDifference = moment().diff(moment(realTime), "minutes");
+// console.log("Difference in time: " + timeDifference);
+
+// var minutesAway = fRequency - minutesAway;
+// console.log("minutes till Train: " + minutesAway);
+
+
+// var tillNext = timeDifference % fRequency;
+// console.log(tillNext);
+
+// // Minute Until Train
+// var tillNext = fRequency - tillNext;
+// console.log("MINUTES TILL TRAIN: " + tillNext);
+
+// // Next Train
+// var nextTrain = moment().add(tillNext, "minutes");
+// console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
